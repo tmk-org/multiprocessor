6fe963584c09f406c1f1b9a3a204bbf54898ee53
kat <emosseyko@lanit-tercom.com>
2021-03-10 00:34:41 +0300

    Remove old paths

--- proc_server/pipeline.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PIPELINE_H
-#define _PIPELINE_H
-
-#ifdef PIPELINE
-    void test_process(void *shmem, int *p_exit_flag);
-#endif
-
-#endif //_PIPELINE_H
--- proc_server/streamer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STREAMER_H
-#define _STREAMER_H
-
-#ifdef STREAMER
-    void test_process(void *shmem, int *p_exit_flag);
-#endif
-
-#endif //_STREAMER_H
